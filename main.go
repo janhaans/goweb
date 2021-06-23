e87cd89d129081fffe6e04be7e35380ac79b7598
Jan Haans <jan.haans@gmail.com>
2021-06-23 13:52:11 +0200

    Add gorilla/mux router

--- main.go
+++ main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
+/*
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	if r.URL.Path == "/" {
@@ -21,8 +24,30 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 			"invalid page.</p>")
 	}
 }
+*/
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1>Welcome to my awesome site</h1>")
+}
+
+func contact(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "To get in touch, please send an email "+
+		"to <a href=\"mailto:support@lenslocked.com\">"+
+		"support@lenslocked.com</a>.")
+}
+
+func error(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprint(w, "<h1>We could not find the page you were looking for :(</h1>"+
+		"<p>Please email us if you keep being sent to an invalid page.</p>")
+}
 
 func main() {
-	http.HandleFunc("/", handlerFunc)
-	http.ListenAndServe(":3000", nil)
+	r := mux.NewRouter()
+	r.HandleFunc("/", home)
+	r.HandleFunc("/contact", contact)
+	r.NotFoundHandler = http.HandlerFunc(error)
+	http.ListenAndServe(":3000", r)
 }
