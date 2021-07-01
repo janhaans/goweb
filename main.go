9a8f8544cc9047ad4c1616f1245e7dbfd2bc10f7
Jan Haans <jan.haans@gmail.com>
2021-07-01 21:08:23 +0200

    Add Users type

--- main.go
+++ main.go
@@ -5,12 +5,12 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/janhaans/goweb/controllers"
 	"github.com/janhaans/goweb/views"
 )
 
 var homeView *views.View
 var contactView *views.View
-var signupView *views.View
 
 func must(err error) {
 	if err != nil {
@@ -28,11 +28,6 @@ func contact(w http.ResponseWriter, r *http.Request) {
 	must(contactView.Render(w, nil))
 }
 
-func signup(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	must(signupView.Render(w, nil))
-}
-
 func notFound(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusNotFound)
 	fmt.Fprint(w, "<h1>We could not find the page you were looking for :(</h1>"+
@@ -43,12 +38,12 @@ func main() {
 
 	homeView = views.NewView("bootstrap", "views/home.gohtml")
 	contactView = views.NewView("bootstrap", "views/contact.gohtml")
-	signupView = views.NewView("bootstrap", "views/signup.gohtml")
+	usersC := controllers.NewUsers()
 
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
-	r.HandleFunc("/signup", signup)
+	r.HandleFunc("/signup", usersC.New)
 	r.NotFoundHandler = http.HandlerFunc(notFound)
 	http.ListenAndServe(":3000", r)
 }
